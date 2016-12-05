6e96eaa230bcd44f5206fff2f9882623e0b44968
Robert Dale <robert@pscpayroll.com>
2016-12-05 15:07:06 +0000

    Day 05

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ConsoleApplication
 {
@@ -8,6 +10,75 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+            //PartOne();
+            PartTwo();
+        }
+
+        public static void PartOne()
+        {
+            string input = "uqwqemis";
+            bool done = false;
+            string result = "";
+            
+            MD5 hasher = MD5.Create();
+
+            int i = 0;
+            while (!done)
+            {
+                var hashbytes = hasher.ComputeHash(Encoding.UTF8.GetBytes(input + (int)i));
+                var hash = BitConverter.ToString(hashbytes).Replace("-", ""); 
+
+                if (hash.StartsWith("00000"))
+                {
+                    result += hash.Substring(5, 1);
+                    Console.WriteLine($"{result} - {i}");
+                    
+                    if (result.Length == 8)
+                        done = true;
+                }
+
+                i++;
+            }
+
+            hasher.Dispose();
+        }
+
+        public static void PartTwo()
+        {
+            string input = "uqwqemis";
+            bool done = false;
+            char[] result = {'x','x','x','x','x','x','x','x'};
+            
+            MD5 hasher = MD5.Create();
+
+            int i = 0;
+            while (!done)
+            {
+                var hashbytes = hasher.ComputeHash(Encoding.UTF8.GetBytes(input + (int)i));
+                var hash = BitConverter.ToString(hashbytes).Replace("-", ""); 
+
+                if (hash.StartsWith("00000"))
+                {
+                    char pwc = hash.Substring(6, 1).ToCharArray()[0];
+                    int position = 10;
+                    try {position = int.Parse(hash.Substring(5, 1));} catch {}
+                    
+                    if(position <= 7)
+                    {
+                        if (result[position] == 'x')
+                            result[position] = pwc;
+                    }
+
+                    Console.WriteLine($"{i} - {new string(result)}");
+                    
+                    if (!new string(result).Contains("x"))
+                        done = true;
+                }
+
+                i++;
+            }
+
+            hasher.Dispose();
         }
     }
 }
