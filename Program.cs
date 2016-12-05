4ba66de2aea9f38d0bf1e5b3a79b69d61dcaefa9
Robert Dale <robert@pscpayroll.com>
2016-12-05 15:19:50 +0000

    uncomment

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            //PartOne();
+            PartOne();
             PartTwo();
         }
 
